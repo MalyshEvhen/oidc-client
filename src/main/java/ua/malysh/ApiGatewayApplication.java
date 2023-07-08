10369469e95ff92a73720073c0055cd13e50bcde
Evhen <malysh.evgeniy@gmail.com>
2023-07-08 16:09:56 +0300

    rename application and add discovery starter

--- src/main/java/ua/malysh/ApiGatewayApplication.java
+++ src/main/java/ua/malysh/ApiGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OidcClientApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OidcClientApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
